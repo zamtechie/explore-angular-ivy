ff185f1b1d8b928116d92a4bfea0f08c9a9e9aa0
zamtechie <zameer.inamdar2008@gmail.com>
2022-01-26 20:56:09 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, VERSION } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map, filter } from 'rxjs';
 
 @Component({
   selector: 'my-app',
@@ -7,4 +9,20 @@ import { Component, VERSION } from '@angular/core';
 })
 export class AppComponent {
   name = 'Angular  ' + VERSION.major;
+
+  constructor() {}
 }
+
+function asycStream(obv: any) {
+  console.log('a');
+  /*setInterval(() => {
+    obv.next(Math.random());
+  }, 5000);*/
+}
+
+// let observerObj = Observable.create(asycStream);
+let observerObj = new Observable(asycStream);
+
+observerObj.subscribe((res) => {
+  console.log(res);
+});
